a3d2f395e01a08c0f2367aacbb9f46a0637ba31e
ulander-a <alve.ulander@chasacademy.se>
2018-01-23 20:31:44 +0100

    can now add custom cards with text input

--- js/kanban.js
+++ js/kanban.js
@@ -1,9 +1,32 @@
 $(document).ready(function (){
 
-    $("#addbutton").on("click", function() {
-        $(this).parent().next(".list-body").append(
-            "<div class='todo-item'>Test</div>"
-        );
+    // Define card
+    function card(color, text) {
+        return `<div class="kanban-card d-flex flex-column" style="background-color:`+color+`">
+        <button class="deletebutton">
+            <span class="oi oi-minus" title="minus" aria-hidden="true"></span>
+        </button>
+        <p>`+text+`</p>
+        </div>`
+    }
+
+    // Add new card
+    $("input[type=text]").keypress(function(event) {
+        if (event.which === 13) {
+            let text = $(this).val();
+            let color = "peachpuff";
+            $(this).next(".list-body").append(
+                card(color, text)
+            )
+        }
+    });
+
+    // Delete card
+    $(".kanban-card").on("click", "button", function() {
+        $(this).parent().fadeOut(250, function(){
+            $(this).remove();
+          });
     });
 
 });
+
