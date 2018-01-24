15fa02be5fa4ad4105402645a4650ba1306d7493
ulander-a <alve.ulander@chasacademy.se>
2018-01-24 01:39:10 +0100

    add dialog

--- js/widgets.js
+++ js/widgets.js
@@ -1,5 +1,36 @@
-$( function() {
-    $( "#sortable1, #sortable2, #sortable3" ).sortable({
+$(document).ready(function () {
+
+  // Init Sortable
+  $(function () {
+    $("#sortable1, #sortable2, #sortable3").sortable({
       connectWith: ".list-body"
     }).disableSelection();
-  } );
+  });
+
+  // Init Dialog
+  $(function () {
+    $("#dialog-confirm").dialog({
+      resizable: false,
+      autoOpen: false,
+      height: "auto",
+      width: 400,
+      modal: true,
+      dialogClass: "no-close",
+      buttons: {
+        "Delete all items": function () {
+          $(".kanban-card").remove();
+          $(this).dialog("close");
+        },
+        Cancel: function () {
+          $(this).dialog("close");
+        }
+      }
+    });
+  });
+
+  $(".menu > button").on("click", function() {
+    $("#dialog-confirm").dialog("open");
+  });
+
+});
+
